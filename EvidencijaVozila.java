43d3bf7afd3914d30a01465745d5569e769a5d69
Paja5555 <pavlepekez55@gmail.com>
2026-03-02 20:04:38 +0100

    Add files via upload

--- EvidencijaVozila.java
+++ EvidencijaVozila.java
@@ -1,8 +1,9 @@
 package vozilo.evidencija;
 
 import vozilo.Vozilo;
+import vozilo.izuzeci.VoziloException;
 
 public interface EvidencijaVozila {
     public void ispisiPoVlasniku(String vlasnik);
-    public void dodajVozilo(Vozilo v);
+    public void dodajVozilo(Vozilo v) throws VoziloException;
 }   //moraju se ove metode prvo implementirati u nekim klasma da bi mogle da se korsite
